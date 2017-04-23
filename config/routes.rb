7637f6d9d1cd87e1b4b53aed3a372773d89fbace
rhamill1 <rhamill11@gmail.com>
2017-04-23 13:33:17 -0700

    sets up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'static_pages#home'
+
+  get '/', to: 'static_pages#home'
+  get "/login", to: "sessions#new"
+  get "/logout", to: "sessions#destroy"
+  post "/sessions", to: "sessions#create"
+
+  resources :partners
+  resources :companies
 end
