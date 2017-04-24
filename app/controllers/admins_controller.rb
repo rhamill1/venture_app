0c78401093aee663eeed080dda4495a83b6c8b2c
rhamill1 <rhamill11@gmail.com>
2017-04-23 18:22:23 -0700

    adds ability to add partner

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,6 +3,8 @@ class AdminsController < ApplicationController
   before_action :logged_in?
 
   def home
+    @partners = Partner.all
+    @companies = Company.all
 
   end
 
--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,2 +1,26 @@
 class PartnersController < ApplicationController
+
+  def new
+    @partner = Partner.new
+  end
+
+  def create
+    @partner = Partner.create(partner_params)
+    if @partner.save
+      flash[:success] = 'Partner Created!'
+      redirect_to admin_path
+    else
+      flash[:error] = @partner.errors.full_messages.join(' ')
+      render :new
+    end
+  end
+
+  private
+
+  def partner_params
+    params.require(:partner).permit(:name, :title, :twitter, :linkedin, :subtitle, :bio, :home_image, :bio_image, :background_image, :story_image, :story_image_text, :signature_image)
+
+
+  end
+
 end
