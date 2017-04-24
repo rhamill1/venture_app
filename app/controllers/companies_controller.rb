a188d46f65bd39d5fe0629b80060eeb59ef58c52
rhamill1 <rhamill11@gmail.com>
2017-04-23 20:31:05 -0700

    adds admin ability to crud companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,8 @@
 class CompaniesController < ApplicationController
 
+  before_action :logged_in?
+  before_action :get_company, only: [:show, :edit, :update, :destroy]
+
   def new
     @company = Company.new
   end
@@ -15,10 +18,35 @@ class CompaniesController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @company.update(company_params)
+      flash[:success] = 'Company Updated!'
+      redirect_to admin_path
+    else
+      flash[:error] = @company.errors.full_messages.join(' ')
+      render :edit
+    end
+  end
+
+  def destroy
+    @company.destroy
+    redirect_to admin_path
+  end
+
   private
 
     def company_params
       params.require(:company).permit(:name, :logo_image, :background_image, :tag_line, :twitter, :linkedin, :company_link)
+    end
+
+    def get_company
+      @company = Company.find(params[:id])
     end
 
 end
